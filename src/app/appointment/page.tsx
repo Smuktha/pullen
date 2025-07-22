f8e14a047427a56d2b2c0c7e0fa42c5319730e8b
Muktha <115140893+Smuktha@users.noreply.github.com>
2025-07-22 08:02:42 +0530

    Update page.tsx

--- src/app/appointment/page.tsx
+++ src/app/appointment/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-
+ 
 export default function AppointmentPage() {
   const [doctors] = useState([
     { _id: "dr-jason1", name: "DR JASON PHAN" },
